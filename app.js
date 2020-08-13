64a1e22c9b993267e0cdfa763902b4c3d79dd25d
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-08-13 16:59:11 -0700

    delete items
    - can remove items from db and todolist

--- app.js
+++ app.js
@@ -71,6 +71,18 @@ app.post("/", function(req, res){
 
 });
 
+app.post("/delete", function(req,res){
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, function(err){
+    if(!err){
+      console.log("Successfully removed checked item");
+      res.redirect("/");
+    }
+    
+  });
+
+});
+
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
